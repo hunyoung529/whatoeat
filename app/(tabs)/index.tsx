934878736ac741105d6c26ea3e7b4384b6b7fec9
hunyoung529 <hunyoung529@gmail.com>
2024-12-20 14:18:54 +0900

    로그인페이지 수정 중중

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,8 +16,39 @@ export default function LoginScreen() {
       <Input
         placeholder="이메일을 입력해주세요."
         leftIcon={{ type: "feather", name: "mail" }}
+        onChangeText={setEmail}
+        inputContainerStyle={styles.inputContainer}
+        inputStyle={styles.input}
       />
-      <Button />
+      <Input
+        placeholder="비밀번호를 입력해주세요."
+        leftIcon={{ type: "feather", name: "lock" }}
+        secureTextEntry // 비밀번호 가리기
+        value={password}
+        onChangeText={setPassword}
+        inputContainerStyle={styles.inputContainer}
+        inputStyle={styles.input}
+      />
+      <View style={styles.socialButtonsContainer}>
+        <Button
+          title="회원가입"
+          type="clear"
+          titleStyle={styles.socialButtonText}
+          containerStyle={styles.socialButtonContainer}
+        />
+        <Button
+          title="소셜 로그인"
+          type="clear"
+          titleStyle={styles.socialButtonText}
+          containerStyle={styles.socialButtonContainer}
+        />
+        <Button
+          title="소셜 로그인"
+          type="clear"
+          titleStyle={styles.socialButtonText}
+          containerStyle={styles.socialButtonContainer}
+        />
+      </View>
     </View>
   );
 }
@@ -25,12 +56,50 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "white",
   },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginTop: 20,
+  logo: {
+    width: 200,
+    height: 100,
+    marginBottom: 20,
+    alignSelf: "center",
+    resizeMode: "contain",
+  },
+  inputContainer: {
+    backgroundColor: "#f2f2f2",
+    borderRadius: 8,
+    borderBottomWidth: 0,
     marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  input: {
+    fontSize: 16,
+  },
+  loginButton: {
+    borderRadius: 8,
+    marginBottom: 20,
+    backgroundColor: "#4CAF50", // 초록색
+    paddingVertical: 12,
+  },
+  socialButtonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  socialButtonContainer: {
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 15,
+    width: 80,
+    height: 80,
+  },
+  socialButtonText: {
+    color: "gray",
+    fontSize: 12,
+    textAlign: "center",
   },
 });
