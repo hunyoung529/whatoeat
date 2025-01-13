fc0fadcf73c662aa33f2f18d13007ce66efec0c0
ys <yesolg90@gmail.com>
2025-01-13 18:20:16 +0900

    댓글 페이지 서버 작업중...

---
+++ backend/routes/reply.js
@@ -0,0 +1,19 @@
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'myProject';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
